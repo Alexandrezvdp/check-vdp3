ef44a3a9497c4126b491eb03545a282c1efb01da
Alexandrezvdp <servicetechniquevdp@gmail.com>
2026-09-18 16:07:01 +0200

    Remove trailing slash from supabaseUrl

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Ne mettez JAMAIS la clé "service_role" ici.
 // Utilisez la clé publishable/anon destinée au navigateur.
 const VDP_CONFIG = {
-  supabaseUrl: "https://ydijhvaghruxfscanntt.supabase.co/",
+  supabaseUrl: "https://ydijhvaghruxfscanntt.supabase.co",
   supabaseKey: "sb_publishable_iuG0AxLuwU-TZ8HRX4R1Dw__uOok8o-",
   alertEmail: "a.zinzius@vedettesdeparis.com"
 };

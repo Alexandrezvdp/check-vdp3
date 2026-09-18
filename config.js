0b99afe0404fce5388410dc5cc8b3f8f25e2a8ad
Alexandrezvdp <servicetechniquevdp@gmail.com>
2026-09-18 16:13:54 +0200

    Update Supabase configuration with new credentials

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Ne mettez JAMAIS la clé "service_role" ici.
 // Utilisez la clé publishable/anon destinée au navigateur.
 const VDP_CONFIG = {
-  supabaseUrl: "https://VOTRE-PROJET.supabase.co",
-  supabaseKey: "VOTRE-CLE-PUBLISHABLE",
+  supabaseUrl: "https://ydijhvaghruxfscanntt.supabase.co",
+  supabaseKey: "sb_publishable_iuG0AxLuwU-TZ8HRX4R1Dw__uOok8o-",
   alertEmail: "a.zinzius@vedettesdeparis.com"
 };
